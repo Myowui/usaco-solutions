53b89e11a0dbc9ab250abc3c1c9cf5e576965733
Myowui <arshiagirdhar22@gmail.com>
2026-09-09 18:12:34 +0530

    Bronze/Simulation: Speeding Ticket

---
+++ bronze/simulation/speedlimit.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+const int LEN = 100;
+
+int main() {
+    freopen ("speeding.in", "r", stdin);
+    freopen ("speeding.out", "w", stdout);
+
+    int N, M;
+    cin >> N >> M;
+
+    int start = 0;
+    vector<int> limit(LEN);
+    for (int i =0; i<N; i++) {
+        int length, speed;
+        cin >> length >> speed;
+        for (int j = start; j<start+length; j++){ limit[j] = speed;}
+        start += length;
+
+    }
+
+    start = 0;
+    vector<int> bessie(LEN);
+    for (int i = 0; i<M; i++){
+        int length, speed;
+        cin >> length >> speed;
+        for (int j = start; j< start +length; j++ ){ bessie[j] = speed;}
+        start += length;
+    }
+    
+    int ans =0;
+    for (int i =0; i<LEN; ++i){
+        if (bessie[i] > limit[i] ) {
+            ans = max(ans, bessie[i] - limit[i]);
+        }
+    }
+    cout << ans;
+}
